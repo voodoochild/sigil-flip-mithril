7438ea1806cc96ceba40419a4570d87ec93dba87
Kriss Watt <kriss.watt@gmail.com>
2015-11-01 07:03:33 -0800

    Reformatted JS to meet ANet JSCS guidelines

--- src/javascript/collection.js
+++ src/javascript/collection.js
@@ -1,25 +1,26 @@
-'use strict';
+"use strict";
 
 class Collection {
-  constructor (items=[], keys=[]) {
-    this.items = items;
-    this.indexes = {};
-    keys.forEach(this.addIndex.bind(this));
-  }
+    constructor(items=[], keys=[]) {
+        this.items = items;
+        this.indexes = {};
+        keys.forEach(this.addIndex.bind(this));
+    }
 
-  addIndex (key) {
-    let index = {};
-    this.items.forEach((item, i) => index[item[key]] = i);
-    this.indexes[key] = index;
-  }
+    addIndex(key) {
+        let index = {};
+        
+        this.items.forEach((item, i) => index[item[key]] = i);
+        this.indexes[key] = index;
+    }
 
-  keys (index) {
-    return Object.keys(this.indexes[index]);
-  }
+    keys(index) {
+        return Object.keys(this.indexes[index]);
+    }
 
-  item (index, key) {
-    return this.items[this.indexes[index][key]];
-  }
+    item(index, key) {
+        return this.items[this.indexes[index][key]];
+    }
 }
 
 module.exports = Collection;
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,81 +1,87 @@
 ;(() => {
-  'use strict';
+    "use strict";
 
-  const m = require('mithril');
-  const util = require('./util');
-  const Collection = require('./collection');
-  const GW2_API = 'https://api.guildwars2.com/v2';
+    const m = require("mithril"),
+        util = require("./util"),
+        Collection = require("./collection"),
+        GW2_API = "https://api.guildwars2.com/v2",
+        TICK = 60000;
 
-  let Weapons = new Collection(require('./weapons.json'), ['id']);
-  let Sigils = new Collection(require('./sigils.json'), ['id', 'name']);
+    let Weapons = new Collection(require("./weapons.json"), [ "id" ]),
+        Sigils = new Collection(require("./sigils.json"), [ "id", "name" ]),
+        ids = []
+            .concat(Weapons.keys("id"), Sigils.keys("id"))
+            .filter(id => id > 0);
 
-  let ids = []
-    .concat(Weapons.keys('id'), Sigils.keys('id'))
-    .filter(id => id > 0);
+    // Grab item details
+    function getItemDetails() {
+        util.batchRequest(`${GW2_API}/items`, ids)
+            .then(items => {
+                items.forEach(item => {
+                    let thing = Weapons.item("id", item.id) ||
+                                Sigils.item("id", item.id);
 
-  // Grab item details
-  function getItemDetails () {
-    util.batchRequest(`${GW2_API}/items`, ids)
-      .then(items => {
-        items.forEach(item => {
-          let thing = Weapons.item('id', item.id) || Sigils.item('id', item.id);
-          thing.icon = item.icon;
-        });
-        m.redraw();
-      });
-  }
-  getItemDetails();
-
-  // Grab prices
-  function getItemPrices () {
-    util.batchRequest(`${GW2_API}/commerce/prices`, ids)
-      .then(prices => {
-        prices.forEach(item => {
-          let thing = Weapons.item('id', item.id) || Sigils.item('id', item.id);
-          thing.buyPrice = item.buys.unit_price;
-          thing.sellPrice = item.sells.unit_price;
-        });
-        m.redraw();
-      });
-  }
-  getItemPrices();
-
-  // Render the UI
-  let SigilFlip = {
-    controller: function () {
-      this.interval = null;
-      this.start = () => this.interval = setInterval(getItemPrices, 60000);
-      this.stop = () => clearInterval(this.interval);
-      this.start();
-    },
-
-    view: function (ctrl) {
-      return m('ul.items', Weapons.items.filter(weapon => weapon.id > 0).map(weapon => {
-        let sigil = Sigils.item('name', weapon.sigil);
-        let profit;
-
-        if (weapon.buyPrice && sigil.sellPrice) {
-          profit = util.flip(weapon.buyPrice, sigil.sellPrice);
-        }
-
-        return m(profit > 0 ? 'li.profitable' : 'li', [
-          m('a', {href: `http://www.gw2spidy.com/item/${weapon.id}`}, [
-            weapon.icon ? m('img.icon', {src: weapon.icon}) : '',
-            m('span', `${weapon.name}: `),
-            weapon.buyPrice ? m('span', `${util.formatPrice(weapon.buyPrice)}`) : '',
-          ]),
-          m('a', {href: `http://www.gw2spidy.com/item/${sigil.id}`}, [
-            sigil.icon ? m('img.icon', {src: sigil.icon}) : '',
-            m('span', `${sigil.name}: `),
-            sigil.sellPrice ? m('span', `${util.formatPrice(sigil.sellPrice)}`) : '',
-          ]),
-          profit ? m('p', 'Profit: ', [
-            m(profit > 0 ? 'span.positive' : 'span.negative', `${util.formatPrice(profit)}`)
-          ]) : '',
-        ]);
-      }));
+                    thing.icon = item.icon;
+                });
+                m.redraw();
+            });
     }
-  };
 
-  m.module(document.getElementById('app'), SigilFlip);
+    getItemDetails();
+
+    // Grab prices
+    function getItemPrices() {
+        util.batchRequest(`${GW2_API}/commerce/prices`, ids)
+            .then(prices => {
+                prices.forEach(item => {
+                    let thing = Weapons.item("id", item.id) ||
+                                Sigils.item("id", item.id);
+                    
+                    thing.buyPrice = item.buys.unit_price;
+                    thing.sellPrice = item.sells.unit_price;
+                });
+                m.redraw();
+            });
+    }
+
+    getItemPrices();
+
+    // Render the UI
+    let SigilFlip = {
+        controller : function() {
+            this.interval = null;
+            this.start = () => this.interval = setInterval(getItemPrices, TICK);
+            this.stop = () => clearInterval(this.interval);
+            this.start();
+        },
+
+        view : function() {
+            return m("ul.items", Weapons.items.filter(weapon => weapon.id > 0).map(weapon => {
+                let sigil = Sigils.item("name", weapon.sigil),
+                    profit;
+
+                if(weapon.buyPrice && sigil.sellPrice) {
+                    profit = util.flip(weapon.buyPrice, sigil.sellPrice);
+                }
+
+                return m(profit > 0 ? "li.profitable" : "li", [
+                    m("a", { href : `http://www.gw2spidy.com/item/${weapon.id}` }, [
+                        weapon.icon ? m("img.icon", { src : weapon.icon }) : "",
+                        m("span", `${weapon.name}: `),
+                        weapon.buyPrice ? m("span", `${util.formatPrice(weapon.buyPrice)}`) : ""
+                    ]),
+                    m("a", { href : `http://www.gw2spidy.com/item/${sigil.id}` }, [
+                        sigil.icon ? m("img.icon", { src : sigil.icon }) : "",
+                        m("span", `${sigil.name}: `),
+                        sigil.sellPrice ? m("span", `${util.formatPrice(sigil.sellPrice)}`) : ""
+                    ]),
+                    profit ? m("p", "Profit: ", [
+                        m(profit > 0 ? "span.positive" : "span.negative", `${util.formatPrice(profit)}`)
+                    ]) : ""
+            ]);
+            }));
+        }
+    };
+
+    m.module(document.getElementById("app"), SigilFlip);
 })();
